11238b77b63337aab8cfc16b809dcafade0359c2
Tom McKernan <30330917+mckernant1@users.noreply.github.com>
2020-06-28 13:10:30 -0700

    adding complete

--- src/main/kotlin/com/github/mckernant1/runner/Help.kt
+++ src/main/kotlin/com/github/mckernant1/runner/Help.kt
@@ -6,5 +6,5 @@ fun printHelp(event: MessageReceivedEvent) {
     event.channel.sendMessage("""
         !help -> lists this menu
         !schedule <league> [number of days] -> get the schedule for a given league. Ex: !schedule lcs 10 gets next 10 matches in lcs
-    """.trimIndent())
+    """.trimIndent()).complete()
 }
