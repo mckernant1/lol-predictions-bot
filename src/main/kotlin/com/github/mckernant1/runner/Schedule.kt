f50c3f28c4fba17b62f988843f5859805c6552d9
Tom McKernan <30330917+mckernant1@users.noreply.github.com>
2020-06-28 22:13:36 -0700

    addin date

--- src/main/kotlin/com/github/mckernant1/runner/Schedule.kt
+++ src/main/kotlin/com/github/mckernant1/runner/Schedule.kt
@@ -2,6 +2,7 @@ package com.github.mckernant1.runner
 
 import com.github.mckernant1.lolapi.schedule.Match
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import java.text.DateFormat
 
 fun scheduleCmd(
     words: List<String>,
@@ -16,9 +17,10 @@ fun scheduleCmd(
 
 private fun formatReply(matches: List<Match>, numToGet: Int, region: String): String {
     val sb = StringBuilder()
+    val dateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.LONG)
     sb.appendln("The next $numToGet matches in $region are: ")
     matches.forEach {
-        sb.appendln("${it.team1} vs ${it.team2}")
+        sb.appendln("${dateFormat.format(it.date)}: ${it.team1} vs ${it.team2}")
     }
     return sb.toString()
 }
