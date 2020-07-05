c619be917bf59a368952603fed7e2f1c5337cae6
Tom McKernan <30330917+mckernant1@users.noreply.github.com>
2020-07-05 14:06:31 -0700

    help to info

--- src/main/kotlin/com/github/mckernant1/runner/Help.kt
+++ src/main/kotlin/com/github/mckernant1/runner/Help.kt
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
 fun printHelp(event: MessageReceivedEvent) {
     event.channel.sendMessage("""
-        !help -> lists this menu
+        !info -> lists this menu
         !schedule <league> [number of matches] -> league is required. matches default is 3
         !results <league> [number of matches] -> league is required. matches default is 3
         !standings <league> -> league is required
