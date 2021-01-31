005e811caa38ecd24bda3ef7efb0ecb3f98b707d
Tom McKernan <30330917+mckernant1@users.noreply.github.com>
2021-01-30 20:31:02 -0800

    fixing back

--- src/main/kotlin/com/github/mckernant1/lol/blitzcrank/utils/JDAHelper.kt
+++ src/main/kotlin/com/github/mckernant1/lol/blitzcrank/utils/JDAHelper.kt
@@ -3,11 +3,11 @@ package com.github.mckernant1.lol.blitzcrank.utils
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
-fun reactInternalError(m: Message): Void = m.addReaction("❌").complete()
+fun reactInternalError(m: Message): Unit = m.addReaction("❌").queue()
 
-fun reactUserError(m: Message): Void = m.addReaction("⛔").complete()
+fun reactUserError(m: Message): Unit = m.addReaction("⛔").queue()
 
-fun reactUserOk(m: Message): Void = m.addReaction("\uD83D\uDC4C").complete()
+fun reactUserOk(m: Message): Unit = m.addReaction("\uD83D\uDC4C").queue()
 
 fun getServerIdOrUserId(event: MessageReceivedEvent): String = try {
     event.guild.id
