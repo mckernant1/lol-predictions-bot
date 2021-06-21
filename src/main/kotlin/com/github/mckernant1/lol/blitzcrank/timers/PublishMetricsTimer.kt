df31d203b18cdc13f8113cffe780269222aa7439
Tom McKernan <30330917+mckernant1@users.noreply.github.com>
2021-06-20 18:12:33 -0700

    catching timers

--- src/main/kotlin/com/github/mckernant1/lol/blitzcrank/timers/PublishMetricsTimer.kt
+++ src/main/kotlin/com/github/mckernant1/lol/blitzcrank/timers/PublishMetricsTimer.kt
@@ -3,11 +3,20 @@ package com.github.mckernant1.lol.blitzcrank.timers
 import com.github.mckernant1.lol.blitzcrank.utils.cwp
 import com.github.mckernant1.lol.blitzcrank.utils.globalTimer
 import net.dv8tion.jda.api.JDA
+import org.slf4j.LoggerFactory
 import java.time.Duration
 import kotlin.concurrent.scheduleAtFixedRate
 
+private val logger by lazy {
+    LoggerFactory.getLogger("BotMetrics")
+}
+
 fun publishBotMetrics(bot: JDA) {
     globalTimer.scheduleAtFixedRate(0, Duration.ofMinutes(5).toMillis()) {
-        cwp.putNumServers(bot.guilds.size)
+        runCatching {
+            cwp.putNumServers(bot.guilds.size)
+        }.onFailure {
+            logger.error("An error occurred while posting metrics")
+        }
     }
 }
--- src/main/kotlin/com/github/mckernant1/lol/blitzcrank/timers/Reminders.kt
+++ src/main/kotlin/com/github/mckernant1/lol/blitzcrank/timers/Reminders.kt
@@ -3,31 +3,40 @@ package com.github.mckernant1.lol.blitzcrank.timers
 import com.github.mckernant1.lol.blitzcrank.model.UserSettings
 import com.github.mckernant1.lol.blitzcrank.utils.globalTimer
 import net.dv8tion.jda.api.JDA
+import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.time.Instant
 import kotlin.concurrent.scheduleAtFixedRate
 import kotlin.streams.asSequence
 
+private val logger by lazy {
+    LoggerFactory.getLogger("ReminderChecker")
+}
+
 fun reminderChecker(bot: JDA) {
     globalTimer.scheduleAtFixedRate(0, Duration.ofMinutes(5).toMillis()) {
-        UserSettings.scan().asSequence().flatMap { it.reminders }
-            .filter { it.shouldSendMessage() }
-            .onEach {
-                val user = bot.getUserById(it.userId) ?: return@onEach
-                val privateChannel = user.openPrivateChannel().complete() ?: return@onEach
-                privateChannel.sendMessage(
-                    "Reminder: ${it.leagueSlug} is coming up in ${it.hoursBeforeMatches} hours"
-                ).complete()
+        runCatching {
+            UserSettings.scan().asSequence().flatMap { it.reminders }
+                .filter { it.shouldSendMessage() }
+                .onEach {
+                    val user = bot.getUserById(it.userId) ?: return@onEach
+                    val privateChannel = user.openPrivateChannel().complete() ?: return@onEach
+                    privateChannel.sendMessage(
+                        "Reminder: ${it.leagueSlug} is coming up in ${it.hoursBeforeMatches} hours"
+                    ).complete()
 
-            }.groupBy { it.userId }
-            .forEach { (userId, reminders) ->
-                val user = UserSettings.getSettingsForUser(userId)
-                val newReminders = reminders.map {
-                    it.lastReminderSentEpochMillis = Instant.now().toEpochMilli()
-                    it
-                }.toMutableList()
-                user.reminders = newReminders
-                UserSettings.putSettings(user)
-            }
+                }.groupBy { it.userId }
+                .forEach { (userId, reminders) ->
+                    val user = UserSettings.getSettingsForUser(userId)
+                    val newReminders = reminders.map {
+                        it.lastReminderSentEpochMillis = Instant.now().toEpochMilli()
+                        it
+                    }.toMutableList()
+                    user.reminders = newReminders
+                    UserSettings.putSettings(user)
+                }
+        }.onFailure {
+            logger.error("An error occurred while sending reminders", it)
+        }
     }
 }
