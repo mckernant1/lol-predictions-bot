89393efcd406ccc6bf67bf7a29935ae69fcd93f7
Thomas McKernan <tmeaglei@gmail.com>
2022-08-14 15:18:47 -0700

    fixing math behind getting the tournament to display for a given league

--- src/main/kotlin/com/github/mckernant1/lol/blitzcrank/utils/ApiExtensions.kt
+++ src/main/kotlin/com/github/mckernant1/lol/blitzcrank/utils/ApiExtensions.kt
@@ -6,21 +6,29 @@ import com.github.mckernant1.lol.esports.api.models.Match
 import com.github.mckernant1.lol.esports.api.models.Tournament
 import org.slf4j.LoggerFactory
 import java.text.SimpleDateFormat
+import java.time.Duration
 import java.time.Instant
+import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 import java.util.Date
 
 private val apiLogger = LoggerFactory.getLogger("ApiLogger")
 
 fun DefaultApi.getMostRecentTournament(league: String): Tournament {
-    return this.getTournamentsForLeague(league.uppercase())
-        .filter { it.startDateAsDate()
-            ?.toInstant()
-            ?.minus(7, ChronoUnit.DAYS)
-            ?.isBefore(Instant.now())
-            ?: false
-        }.maxByOrNull { it.startDateAsDate()!! }
-        ?: error("Could not get a tournament for league '$league'")
+    val tournamentsByMostRecent = this.getTournamentsForLeague(league.uppercase())
+        .filter {
+            it.startDateAsDate()
+                ?.minus(7, ChronoUnit.DAYS)
+                ?.isBefore(Instant.now())
+                ?: false
+        }.sortedByDescending { it.startDateAsDate()!! }
+
+    val mostRecentTournament = tournamentsByMostRecent.find { it.isOngoing() }
+        ?: tournamentsByMostRecent.first()
+
+    apiLogger.info("Most recent tournament for ${league.uppercase()} is ${mostRecentTournament.tournamentId}")
+
+    return mostRecentTournament
 }
 
 fun List<Match>.filterPastMatches() = filter {
@@ -31,17 +39,24 @@ fun List<Match>.filterFutureMatches() = filter {
     it.startTimeAsInstant().isAfter(Instant.now())
 }
 
-fun Tournament.startDateAsDate(): Date? {
+fun Tournament.isOngoing(): Boolean {
+    val now = Instant.now().truncatedTo(ChronoUnit.DAYS)
+    return this.startDateAsDate().orEpoch() <= now && now <= this.endDateAsDate().orEpoch()
+}
+
+fun Tournament.startDateAsDate(): Instant? {
     return try {
-        SimpleDateFormat("yyyy-MM-dd").parse(this.startDate)
+        SimpleDateFormat("yyyy-MM-dd").parse(this.startDate).toInstant()
     } catch (e: Exception) {
         null
     }
 }
 
-fun Tournament.endDateAsDate(): Date? {
+private fun Instant?.orEpoch(): Instant = this ?: Instant.EPOCH
+
+fun Tournament.endDateAsDate(): Instant? {
     return try {
-        SimpleDateFormat("yyyy-MM-dd").parse(this.endDate)
+        SimpleDateFormat("yyyy-MM-dd").parse(this.endDate).toInstant()
     } catch (e: Exception) {
         null
     }
